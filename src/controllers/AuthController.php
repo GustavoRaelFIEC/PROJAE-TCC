aee5fdeabb5e7035ae1c82627919a1a8a18dd116
Davi Lopes <davi41933@fiec.edu.br>
2026-03-04 17:23:03 -0300

    Merge branch 'main' of https://github.com/GustavoRaelFIEC/PROJAE-TCC

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -3,15 +3,18 @@
 require_once './src/config/database.php';
 require_once './src/models/User.php';
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_GET['action'] ?? '';
 
-    if($action === 'login'){
+    if ($action === 'login') {
         handleLogin($pdo);
     }
 }
 
-function handleLogin($pdo){
+function handleLogin($pdo)
+{
+    Session::start();
+
 
     $email = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL);
     $senha = $_POST['senha'] ?? '';
@@ -19,24 +22,25 @@ function handleLogin($pdo){
     //Validações
     $errors = [];
 
-    if(!Security::validateEmail(($email))){
+    if (!Security::validateEmail(($email))) {
         $errors[] = "Email inválido!";
     }
 
-    if(!Security::validatePassword($senha)){
-        $errors[] = "A senha deve ter pelo menos 2 caracteres";
+    if (!Security::validatePassword($senha)) {
+        $errors[] = "A senha deve ter pelo menos 8 caracteres";
     }
 
 
-    if(empty($errors)){
-        try{
+    if (!empty($errors)) {
+        return;
+    }
+    
+    try {
         $userModel = new User($pdo);
 
         $usuario = $userModel->findByEmail($email);
-
-    }catch(PDOException $e){
+    } catch (PDOException $e) {
         error_log("Erro no login: " . $e->getMessage());
         $errors[] = "Erro no sistema. Volte mais tarde.";
     }
-}
 }
