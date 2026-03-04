ab8f12948b7566aff0e919262986047f9614a564
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-03-04 16:26:36 -0300

    login e AuthController

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -4,5 +4,39 @@ require_once './src/config.php';
 require_once './src/models/user.php';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    
+    $action = $_GET['action'] ?? '';
+
+    if($action === 'login'){
+        handleLogin($pdo);
+    }
+}
+
+function handleLogin($pdo){
+
+    $email = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL);
+    $senha = $_POST['senha'] ?? '';
+
+    //Validações
+    $errors = [];
+
+    if(!Security::validateEmail(($email))){
+        $errors[] = "Email inválido!";
+    }
+
+    if(!Security::validatePassword($senha)){
+        $errors[] = "A senha deve ter pelo menos 2 caracteres";
+    }
+
+
+    if(empty($errors)){
+        try{
+        $userModel = new User($pdo);
+
+        $usuario = $userModel->findByEmail($email);
+
+    }catch(PDOException $e){
+        error_log("Erro no login: " . $e->getMessage());
+        $errors[] = "Erro no sistema. Volte mais tarde.";
+    }
+}
 }
