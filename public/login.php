ffdcd17e39b12372b4c4840fbf2e8a9657637521
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-03-05 15:30:04 -0300

    ajustei para fazer login

--- public/login.php
+++ public/login.php
@@ -14,15 +14,15 @@
         <img class="logo-img" src="assets/img/imagotipo.png" alt="LOGO PROJAE">
         <div class="container">
             <h1 class="titulo">Entrar</h1>
-            <form method="POST" class="form">
+            <form method="POST" action="../src/controllers/LoginController.php" class="form">
                 <label class="input-label" for="email">
                     E-mail
-                    <input class="input" placeholder="Digite seu Email" id="email" type="email" required>
+                    <input class="input" placeholder="Digite seu Email" id="email" name="email" type="email" required>
                 </label>
                 <!-- onde ficará a mensagem de erro da email (span) -->
                 <label class="input-label" for="senha">
                     Sua Senha
-                    <input class="input" placeholder="Digite sua senha" id="senha" type="password" required>
+                    <input class="input" placeholder="Digite sua senha" id="senha" name="senha" type="password" required>
                 </label>
                 <!-- onde ficará a mensagem de erro da senha (span) -->
 
--- src/models/Usuarios.php
+++ src/models/Usuarios.php
@@ -3,15 +3,19 @@
 class User {
     private $pdo;
 
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
     public function findByEmail($email) {
-        $sql = "SELECT pessoas.*, usuarios.email
+        $sql = "SELECT pessoas.*, usuarios.email, usuarios.tipo
                 FROM pessoas
                 INNER JOIN usuarios ON usuarios.id = pessoas.id_usuario
                 WHERE usuarios.email = ?";
 
         $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$email]);
 
-        $stmt->execute($email);
-        return $stmt->fetch();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
