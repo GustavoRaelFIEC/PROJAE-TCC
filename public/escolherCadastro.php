b7f7ed8e5e61ec9075d5ad31b4d75e454952720d
GustavoRaelFIEC <gustavo41938@fiec.edu.br>
2026-03-05 17:44:38 -0300

    começando pagina escolherCadastro, estrura pronta estilização incompleta

---
+++ public/escolherCadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - PROJAE</title>
+
+    <link rel="shortcut icon" href="assets/img/isotipo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/login.css">
+
+</head>
+<body class="corpo">
+    <main class="principal">
+        <img class="logo-img" src="assets/img/imagotipo-removebg.png" alt="LOGO PROJAE">
+        <div class="container">
+            <h1 class="titulo">Cadastrar</h1>
+            <h3>Escolha o tipo de conta que você deseja abrir</h3>
+            <div class="tipoConta">
+                <label for="pessoa">
+                    <button class="btn-pessoa" id="pessoa" name="tipoConta" value="pessoa">Pessoa</button>
+                </label>
+                <p>Pessoa usam a plataforma para buscar e se candidatar a vagas</p>
+                <label  for="empresa">
+                    <button class="btn-empresa" id="empresa" name="tipoConta" value="empresa">Empresa</button>
+                </label>
+                <p>Empresas usam a plataforma para cadastrar e gerenciar vagas</p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
