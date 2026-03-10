ff7aa39b630b68aaf7a90a100c9af8609ed6d243
GustavoRaelFIEC <gustavo41938@fiec.edu.br>
2026-03-10 17:49:41 -0300

    cadastroEmpresa

--- banco.sql
+++ banco.sql
@@ -36,7 +36,8 @@ CREATE TABLE empresas (
     nome VARCHAR(150) NOT NULL,
     cnpj VARCHAR(14) UNIQUE,
     telefone VARCHAR(20),
-    cidade VARCHAR(100),
+    unidadeFederacao VARCHAR(100),
+    uploads VARCHAR(100),
     
     id_usuario INT NOT NULL,
     
---
+++ public/cadastroEmpresa.php
@@ -0,0 +1,85 @@
+<?php
+
+session_start();
+require_once("../PROJAE-TCC/banco.sql");
+
+// Uploads de Fotos
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_FILES["fotoEmpresa"]) && $_FILES["FotoEmpresa"]["error"] == 0) {
+        $pasta = "../public/assets/uploads/";
+        if (!is_dir($pasta)) {
+            mkdir($pasta, 0777, true);
+        }
+        $extensao = pathinfo($_FILES["fotoEmpresa"]["name"],PATHINFO_EXTENSION);
+        $nomeArquivo = uniqid() . "." . $extensao;
+        $caminho = $pasta . $nomeArquivo;
+
+        move_uploaded_file($_FILES['foto']['tmp_name'], $caminho);
+
+        $stmt = $pdo->prepare("UPDATE cliente SET foto = ? WHERE id = ?");
+        $stmt->execute([$caminho, $userID]);
+
+        $usuario['foto'] = $caminho;
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cadastroEmpresa..css">
+    <title>Cadastrar Empresa</title>
+</head>
+<body class="corpo">
+    <header class="cabecalho">
+        <div class="logo"><img src="assets/img/imagotipo.png" alt=""></div>
+        <ul class="list">
+            <li class="item-list">Página Principal</li>
+            <li class="item-list">Sobre</li>
+            <li class="item-list">Ajuda</li>
+        </ul>
+    </header>
+    <main class="principal">
+        <h1 class="titulo">Registrar <span>Empresa</span></h1>
+        <div class="cadastro">
+            <form method="POST" class="fomulario">
+                <legend class="subTitulo">Dados do Usuário</legend>
+
+                <label class="input-label" for="">
+                    Razão Social
+                    <input class="input" placeholder="Insira a Razão Social Registrada" id="" name="" type="text" required>
+                </label>
+
+                <label class="input-label" for="">
+                    CNPJ
+                    <input class="input" placeholder="__-___-___/____-__" id="" name="" type="text" required>
+                </label>
+
+                <label class="input-label" for="">
+                    Telefone
+                    <input class="input" placeholder="(__) _____-____" id="" name="" type="text" required>
+                </label>
+
+                <label class="input-label" for="">
+                    Unidade de Federação
+                    <input class="input" placeholder="Ex: São Paulo - SP" id="" name="" type="text" required>
+                </label>
+
+                <button class="btn-submit" type="submit">Entrar</button>
+            </form>
+            <div class="fotoUpload">
+                <div class="fotoEmpresa">
+                    <h1>Foto de Perfil</h1>
+                    <img src="" alt="">
+                </div>
+                <p>Use uma imagem de boa qualidade, que mostre sua logotipo, ou que reflita sua Razão Social</p>
+                <input class="btn-upload" name="fotoEmpresa" type="file">
+            </div>
+        </div>
+    </main>
+</body>
+</html>
