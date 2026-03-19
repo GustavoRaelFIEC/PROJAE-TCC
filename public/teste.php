4779c0bb6313c4964657f09ee57d74ba34e1ca19
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-03-19 17:06:17 -0300

    testando criar vaga

--- public/teste.php
+++ public/teste.php
@@ -20,7 +20,7 @@
         <main class="principal">
             <h1 class="titulo">Postar <span>Vaga</span></h1>
             <div class="cadastro">
-                <form method="POST" action="../src/controllers/CadastroController.php" class="fomulario">
+                <form method="POST" action="../src/controllers/VagaController.php" class="fomulario">
                     <legend class="subTitulo">Dados do Usuário</legend>
                     <label class="input-label" for="titulo">
                         titulo
--- src/controllers/VagaController.php
+++ src/controllers/VagaController.php
@@ -19,80 +19,32 @@ function handleVaga($pdo)
 
     $dados = getRequestData();
 
-    // Dados do formulário
-    $email = filter_var($dados['email'] ?? '', FILTER_SANITIZE_EMAIL);
-    $senha = $dados['senha'] ?? '';
-    $tipo = $dados['tipo'] ?? '';
-
-    $errors = [];
-
-    // Validações 
-    if (!Security::validateEmail($email)) {
-        header('Location: ../../public/escolherCadastro.php');
-        $errors[] = "❌ Email inválido!";
-    }
-
-    if (!Security::validatePassword($senha)) {
-        header('Location: ../../public/escolherCadastro.php');
-        $errors[] = "❌ A senha deve ter pelo menos 8 caracteres";
-    }
-
-    if (!empty($errors)) {
-        $_SESSION['cadastro_errors'] = $errors;
-        var_dump($errors);
-        exit;
-    }
-
     try {
         $userModel = new Usuario($pdo);
-
-        // Verifica se email já existe
-        $usuario = $userModel->findByEmail($email);
-
-        if ($usuario) {
-            $_SESSION['cadastro_errors'] = ["⚠️ Email já cadastrado!"];
-            header("Location: ../../public/cadastro.php");
-            exit;
-        }
-
-        // Hash da senha
-        $senhaHash = Security::hashPassword($senha);
 
         // Transação segura (banco empresa e pessoa)
         $pdo->beginTransaction();
 
-        // Criar Usuário
-        $userId = $userModel->createUser($email, $senhaHash, $tipo);
+        $dados = [
+            'titulo' => Security::sanitizeInput($dados['titulo'] ?? ''),
+            'descricao' => Security::sanitizeInput($dados['descricao'] ?? ''),
+            'tipo' => Security::sanitizeInput($dados['tipo'] ?? ''),
+            'salario' => Security::sanitizeInput($dados['salario'] ?? ''),
+            'cidade' => Security::sanitizeInput($dados['cidade'] ?? ''),
+            'status' => Security::sanitizeInput($dados['cidade'] ?? '')
+        ];
 
-        // Criar dados específicos (Pessoa e Empresa)
-        if ($tipo === 'pessoa') {
-            $dados = [
-                'nome' => Security::sanitizeInput($dados['nome'] ?? ''),
-                'cpf' => Security::sanitizeInput($dados['cpf'] ?? ''),
-                'telefone' => Security::sanitizeInput($dados['telefone'] ?? ''),
-                'instituicao' => Security::sanitizeInput($dados['instituicao'] ?? ''),
-                'curso' => Security::sanitizeInput($dados['curso'] ?? '')
-            ];
-            $userModel->createPessoa($userId, $dados);
-        } elseif ($tipo === 'empresa') {
-            $dados = [
-                'nome' => Security::sanitizeInput($dados['nome'] ?? ''),
-                'cnpj' => Security::sanitizeInput($dados['cnpj'] ?? ''),
-                'telefone' => Security::sanitizeInput($dados['telefone'] ?? ''),
-                'cidade' => Security::sanitizeInput($dados['cidade'] ?? '')
-            ];
-            $userModel->createEmpresa($userId, $dados);
-        }
+        $userModel->createPessoa($_SESSION['user_id'], $dados);
 
         // Se der tudo certo, confirma tudo
         $pdo->commit();
 
         // Sucesso
-        $_SESSION['sucesso'] = "✅ Conta criada com sucesso!";
-        header("Location: ../../public/login.php");
-        exit;
+        $_SESSION['sucesso'] = "✅ Vaga criada com sucesso!";
+        header("Location: ../views/empresa.php");
+        exit();
     } catch (PDOException $e) {
-        error_log("Erro no cadastro " . $e->getMessage());
+        error_log("Erro ao criar vaga " . $e->getMessage());
         // Se estiver em transaction, o rollBack volta se der alguma coisa errada
         if ($pdo->inTransaction()) {
             $pdo->rollBack();
