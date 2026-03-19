b590228c29d32e8310345dad5feecf9797fec416
Thiago <thiago41962@fiec.edu.br>
2026-03-19 17:42:23 -0300

    comentarios no dashboard e navbar superior(mesma do index)

--- src/views/dashboardEmpresa.php
+++ src/views/dashboardEmpresa.php
@@ -7,6 +7,33 @@
 </head>
 <body>
     <h1>DASH BOARD EMPRESA</h1>
+    <nav class="navegacao">
+            <ul>
+                <li><a class="active" href="#">Principal</a></li>
+                <li><a href=""></a>Sobre</li>
+                <li><a href=""></a>Ajuda</li>
+            </ul>
+            <div class="">
+                <!-- foto e notificaçoes -->
+            </div>
+        </nav>
+        <div>
+            <!-- barra de pesquisa -->
+        </div>
+
+        <div> <!--conteudo principal da pagina -->
+            <main> 
+                <!-- onde irao ficar as vagas da empresa, caso não tenha vagas tera um botão ou link para criar vagas aqui -->
+            </main>
+            <aside>
+                <!-- filtros -->
+            </aside>
+        </div>
+
+        <footer></footer>
+
+        <!-- ainda decidir onde colocar botão de criar vagas -->
+    
     
     <a href="teste.php">teste para postar vaga</a>
 </body>
