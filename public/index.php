640f100984d7e3479c28bcafc3fcceb10a89c901
Rael <gustavo41938@fiec.edu.br>
2026-03-23 00:05:25 -0300

    renomeei, .html para .php

--- public/index.php
+++ public/index.php
@@ -4,26 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="shortcut icon" href="assets/img/isotipo.png" type="image/x-icon">
+
     <link rel="stylesheet" href="assets/css/globalEimports.css">
     <link rel="stylesheet" href="assets/css/navegation.css">
     <link rel="stylesheet" href="assets/css/index.css">
 
-    <title>PROJAE - HOME</title>
+    <title> Ínicio - PROJAE</title>
 </head>
 <body>
     <header class="cabecalho">
-        <div class="logo"><img class="img" src="assets/img/imagotipo.png" alt="Projae logo"></div>
-        <ul class="list">
-            <li><a class="item-list active" href="index.php">Página Principal</a></li>
-            <li><a class="item-list" href="about.php">Sobre</a></li>
-            <li><a class="item-list" href="help.php">Ajuda</a></li>
-        </ul>
-        <div class="cta">
-            <a href="#" class="btnLogin">Entrar</a>
-            <a href="#" class="btnCadastro">Cadastrar</a>
+        <div class="contentCabecalho">
+            <div class="logo"><img class="img" src="assets/img/imagotipo.png" alt="Projae logo"></div>
+            <ul class="list">
+                <li><a class="item-list active" href="" onclick="location.reload()">Página Principal</a></li>
+                <li><a class="item-list" href="about.php">Sobre</a></li>
+                <li><a class="item-list" href="help.php">Ajuda</a></li>
+            </ul>
+            <div class="cta">
+                <a href="login.php" class="btnLogin">Entrar</a>
+                <a href="escolherCadastro.php" class="btnCadastro">Cadastrar</a>
+            </div>
         </div>
     </header>
-    <main class="principal ">
+    <main class="principal">
         <div class="content">
             <div class="sideLeft">
                 <img src="assets/img/background2-index.png" alt="">
@@ -33,6 +37,6 @@
                 <p class="paragrafo">Se <a href="#">Cadastre</a href="#"> ou <a href="#">Entre</a> caso já tenha uma conta.</p>
             </div>
         </div>
-    </main>
+    </main> 
 </body>
 </html>
