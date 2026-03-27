88710ee5299ead23936250bf209dc679c5e4ce00
Pedro Oliveira <pedro41958@fiec.edu.br>
2026-03-27 15:20:10 -0300

    back do BuscarVagas funcionando

---
+++ public/views/testeBuscarVaga.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . "/../../src/controllers/VagaController.php";
+
+$vagas = handleBuscarVaga($pdo);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        foreach($vagas as $vaga):
+    ?>
+    <div style="border: 5px solid black;">
+        <p><?= $vaga['titulo'] ?></p>
+        <p><?= $vaga['descricao'] ?></p>
+        <p><?= $vaga['tipo'] ?></p>
+        <p><?= $vaga['salario'] ?></p>
+        <p><?= $vaga['cidade'] ?></p>
+        <p><?= $vaga['status'] ?></p>
+        <p><?= $vaga['data_publicacao'] ?></p>
+        <p><?= $vaga['nome'] ?></p>
+    </div>
+    <?php
+    endforeach;
+    ?>
+</body>
+</html>
