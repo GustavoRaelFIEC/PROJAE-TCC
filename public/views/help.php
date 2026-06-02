21bf232d4ad15483678a00ac49992a091674a0a8
thiago <thiago41962@fiec.edu.br>
2026-06-02 17:43:34 -0300

    Perguntas e resposta na pagina help

--- public/views/help.php
+++ public/views/help.php
@@ -42,16 +42,58 @@
 
         <div class="conteudo">
             <div class="pergunta">
-                <h2 class="tituloPergunta-1">placeholder</h2>
-                <p class="corpoPergunta-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt cupiditate doloremque beatae voluptatibus unde veniam fugiat explicabo voluptates? Sunt facere temporibus officia praesentium earum repudiandae deleniti id, eaque quas.</p>
+                <h2 class="tituloPergunta-1">Como utilizar o PROJAE</h2>
+                <p class="corpoPergunta-1">Cadastre uma conta e entre no sistema com ela, tendo acesso ao seu dashboard onde você pode fazer diferentes ações dependendo do tipo de seu cadastro</p>
             </div>
             <div class="pergunta">
-                <h2 class="tituloPergunta-2">placeholder 2</h2>
-                <p class="corpoPergunta-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt cupiditate doloremque beatae voluptatibus unde veniam fugiat explicabo voluptates? Sunt facere temporibus officia praesentium earum repudiandae deleniti id, eaque quas.</p>
+                <h2 class="tituloPergunta-2">Como cadastar uma conta</h2>
+                <p class="corpoPergunta-2">
+                    Precione o botão cadastrar no topo da pagina, então escolha seu tipo de conta, pessoa sendo a conta do usuario
+                    que busca o realizar estagio ou uma vaga como joven aprendiz e empresa sendo para corporaçoes buscando oferecer essas vagas,
+                    após escolher o seu tipo de conta preencha o formulario com as informações pedidas e precione o botão cadastrar para completar o cadrasto
+                </p>
             </div>
             <div class="pergunta" >
-                <h2 class="tituloPergunta-1">placeholder 3</h2>
-                <p class="corpoPergunta-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sunt cupiditate doloremque beatae voluptatibus unde veniam fugiat explicabo voluptates? Sunt facere temporibus officia praesentium earum repudiandae deleniti id, eaque quas.</p>
+                <h2 class="tituloPergunta-1">Como entrar em uma conta</h2>
+                <p class="corpoPergunta-1">
+                    Precione o botão entrar no topo da pagina, coloque o email e senha da conta em seus respectivos campos cadastrada e conclua o formulario
+                    precionando o botão entrar
+                </p>
+            </div>
+            <div class="pergunta">
+                <h2 class="tituloPergunta-2">Dashboard pessoa</h2>
+                <p class="corpoPergunta-2">
+                    Ao entrar em uma conta do tipo pessoa, o usuario entra no seu dashboard onde pode se observar as suas informações do cadastro em um card, editar seu perfil e ver as vagas que a pessoa se inscreveu 
+                </p>
+            </div>
+            <div class="pergunta">
+                <h2 class="tituloPergunta-1">Como interagir com vagas inscritas</h2>
+                <p class="corpoPergunta-1">
+                    Cada vaga vista no dashboard de um usuario pessoa pode ser interagida com 2 diferentes botões, "Detalhes", que mostra as informações de 
+                    forma mais detalhada e "Cancelar Inscrição" que cancela a inscreção a essa vaga, a removendo das vagas incritas vista no dashboard
+                </p>
+            </div>
+            <div class="pergunta">
+                <h2 class="tituloPergunta-2">Dashboard empresa</h2>
+                <p class="corpoPergunta-2">
+                    Ao entrar em uma conta do tipo empresa, o usuario entra no seu dashboard onde pode se observar as suas informações do cadastro em um card, editar seu perfil, ver as vagas criadas pela empresa, 
+                    os candidatos inscritos em suas vagas e criar novas vagas  
+                </p>          
+            </div>
+            <div class="pergunta">
+                <h2 class="tituloPergunta-1">Como ver suas vagas e candidatos</h2>
+                <p class="corpoPergunta-1">
+                    Ambas informações estão no dashboard de todos os usuarios do tipo empresa, o usuario pode ver uma dessas informações por vez, abaixo das 
+                    informaçoes do cadastro existem 2 botoes, "Suas Vagas" e "Seus Candidatos", precionar um deles mostra a informação selecionada, precionar o 
+                    outro botão muda a informação na pagina, é possivel interagir com cada vaga ou canditado para ver seus detalhes 
+                </p>
+            </div>
+            <div class="pergunta">
+                <h2 class="tituloPergunta-2">Como criar novas vagas</h2>
+                <p class="corpoPergunta-2">
+                    No canto inferior direito do card com as informações do cadastro existe o botão "Nova Vaga", precione o botão para abrir o formulario de 
+                    criação de vagas, preencha todos os campos e precione publicar para finalizar o formulario e publicar a nova vaga ao nosso sistema
+                </p>
             </div>
         </div>
 
